8b036fa29f82d2341604a63a4fb3638281fee7a1
Jonas-gbt <45062291+Jonas-gbt@users.noreply.github.com>
2020-01-27 09:12:49 +0000

    ajout de l'énigme
    ajout de la scène de l'enigme en cliquant sur le livre puis passage à la scène avec le cristal

--- Escape Game/Assets/Scripts/ClickObject.cs
+++ Escape Game/Assets/Scripts/ClickObject.cs
@@ -9,36 +9,51 @@ public class ClickObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+        if((Input.touchCount > 0) | (Input.GetMouseButtonDown(0)))
         {
             
-            Touch touch = Input.GetTouch(0) ;
-            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            //Touch touch = Input.GetTouch(0) ;
+            //Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
             RaycastHit hit;
             
             
             if( Physics.Raycast(ray, out hit))
             {
                 
-                if (touch.tapCount == 2 /*&& hit.transform.name == "Pass MS"*/)
-                {
+                //if (Input.GetMouseButtonDown(0)/*touch.tapCount == 2 && hit.transform.name == "Pass MS"*/)
+                //{
                     /*Debug.Log(hit.transform.name);
                     SceneManager.LoadScene("the last revelation");*/
                     if (hit.transform.name == "Cube Test")
                     {
-                        SceneManager.LoadScene("Terminal Hacker");
-                    }
+                        SceneManager.LoadScene("Terminal Hacker 1");
+                    }/*
+                    else if (hit.transform.name == "TreasureChest_01 (512)")
+                    {
+                        SceneManager.LoadScene("qcm");
+                    }*/
                     else if(hit.transform.name == "Pass MS")
                     {
-                        SceneManager.LoadScene("the last revelation");
+                        SceneManager.LoadScene("the last revelation 1");
                     }
                     else if (hit.transform.name == "Pass Initial")
                     {
                         SceneManager.LoadScene("Start scene");
                     }
-
+                    else if (hit.transform.name == "BookEnigma")
+                    {
+                        SceneManager.LoadScene("qcm");
+                    }
+                    else if (hit.transform.name == "Zone_crystal")
+                    {
+                        SceneManager.LoadScene("the last revelation 3");
+                    }
+                    
                 }
             }
         }
     }
-}
+//}
---
+++ Escape Game/Assets/Scripts/Qcm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Qcm : MonoBehaviour
+{
+    
+    //public GameObject goodAnswer;
+    //public GameObject wrongAnswer;
+    
+    public void choiceGoodAnswer()
+    {
+        SceneManager.LoadScene("the last revelation 2");
+    }
+
+    public void choiceBadAnswer()
+    {
+        SceneManager.LoadScene("Start scene");
+    }
+
+    
+}
