a2f16f2c400f65a20a92d9bf108c14080aac5c33
robinouuuu <44643556+robinouuuu@users.noreply.github.com>
2020-01-28 09:29:00 +0100

    starting video

--- Escape Game/Assets/Scripts/ClickObject.cs
+++ Escape Game/Assets/Scripts/ClickObject.cs
@@ -13,13 +13,21 @@ public class ClickObject : MonoBehaviour
         {
             
             Touch touch = Input.GetTouch(0) ;
+<<<<<<< Updated upstream
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
+=======
+            Ray ray1 = Camera.main.ScreenPointToRay(touch.position);
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition );
+            
+>>>>>>> Stashed changes
             RaycastHit hit;
             
             
-            if( Physics.Raycast(ray, out hit))
+            if( (Physics.Raycast(ray, out hit)) || (Physics.Raycast(ray1, out hit)))
             {
                 
+<<<<<<< Updated upstream
                 if (touch.tapCount == 2 /*&& hit.transform.name == "Pass MS"*/)
                 {
                     /*Debug.Log(hit.transform.name);
@@ -27,6 +35,18 @@ public class ClickObject : MonoBehaviour
                     if (hit.transform.name == "Cube Test")
                     {
                         SceneManager.LoadScene("Terminal Hacker");
+=======
+                if (Input.GetMouseButtonDown(0) || touch.tapCount == 2 )
+                {
+                    
+                    if (hit.transform.name == "Cube Test")
+                    {
+                        SceneManager.LoadScene("Terminal Hacker 1");
+                    }
+                    else if(hit.transform.name == "Pass MS")
+                    {
+                        SceneManager.LoadScene("the last revelation 1");
+>>>>>>> Stashed changes
                     }
                     else if(hit.transform.name == "Pass MS")
                     {
