8cd17d3bd42e1063c567285f1affc9c9c6f59cd0
Rob1Wagner <wag.rob@hotmail.fr>
2020-03-24 10:50:05 +0100

    Ajout lumière génie + fond du génie

---
+++ Escape Game démo v2/Assets/Scripts/Inutile/Genie.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Genie : MonoBehaviour
+{
+    
+    public bool hasAppeared = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeGenie(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject genie = GameObject.Find("Genius");
+        GameObject smoke = GameObject.Find("Smoke");
+       
+        if((smoke==null) && (hasAppeared)){
+            seeGenie(true);
+            hasAppeared = false;
+        }
+            
+    }
+
+    void seeGenie(bool isVisible){
+        GameObject genieHaut = GameObject.Find("hautGenie");
+        GameObject genieBas = GameObject.Find("basGenie");
+        genieHaut.GetComponent<Renderer>().enabled = isVisible;
+        genieBas.GetComponent<Renderer>().enabled = isVisible;
+    }
+    
+    
+}
---
+++ Escape Game démo v2/Assets/Scripts/PickableObject.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce = 10;
+    private bool hasPlayer = false;
+    private bool beingCarried = false;
+    private bool touched = false;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(gameObject.transform.position, player.position);
+
+        if(distance <= 1.9f)
+        {
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
+        }
+
+        if(hasPlayer && Input.touchCount == 5)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;
+            beingCarried = true;
+        }
+
+        if (beingCarried)
+        {
+            if (touched)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+
+            if(Input.touchCount == 4)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+            }
+
+            if(Input.touchCount == 3)
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+        }
+    }
+    // Détection de contact grace au collider is trigger
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+}
