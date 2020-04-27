4fe6730b730696c1a36a2ffea6c6acadd72caf6d
Hamze <hamzaalrashed1@hotmail.com>
2020-04-27 18:50:31 +0200

    Sprint avril avec l'inventaire

---
+++ Sprint_Avril_Avec_Inventaire/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventory;
+    public GameObject slotHolder;
+
+    private bool inventoryEnabled;
+    private int allSlots;
+    private GameObject[] slot;
+
+    
+
+    void Start()
+    {
+        allSlots = 27;
+        slot = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+
+            if (slot[i].GetComponent<Slot>().item == null)
+            {
+                slot[i].GetComponent<Slot>().empty = true;
+                
+                slot[i].SetActive(false);
+            }
+        }
+    }
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+        if(inventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit)){
+                if (hit.transform.tag == "Item") {
+                    //Debug.Log(hit.transform.name);
+                    GameObject itemPickedUp = hit.transform.gameObject;
+                    Item item = itemPickedUp.GetComponent<Item>();
+                    AddItem(itemPickedUp, item.id, item.type, item.description, item.icon);
+                }
+            } 
+        }
+    }
+
+
+    public void AddItem(GameObject itemObject, int itemId, string itemType, string itemDescription, Sprite itemIcon)
+    {
+        for(int i =0; i < allSlots; i++)
+        {
+            if (slot[i].GetComponent<Slot>().empty)
+            {
+                itemObject.GetComponent<Item>().pickedUp = true;
+
+                slot[i].GetComponent<Slot>().item = itemObject;
+                slot[i].GetComponent<Slot>().icon = itemIcon;
+                slot[i].GetComponent<Slot>().type = itemType;
+                slot[i].GetComponent<Slot>().description = itemDescription;
+                slot[i].GetComponent<Slot>().id = itemId;
+
+                itemObject.transform.parent = slot[i].transform;
+                itemObject.SetActive(false);
+
+                slot[i].GetComponent<Slot>().UpdateSlot();
+                slot[i].GetComponent<Slot>().empty = false;
+                slot[i].SetActive(true);
+                return;
+            }
+            
+        }
+        return;
+    }
+}
---
+++ Sprint_Avril_Avec_Inventaire/Assets/Scripts/Inventory/LinkObjects.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LinkObjects : MonoBehaviour
+{
+    private GameObject itemManager;
+    public int id;
+    private GameObject obj;
+    private GameObject SlotHolder;
+    private GameObject Slot;
+    private GameObject panel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetMouseButtonDown(0)))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if ((Physics.Raycast(ray, out hit)))
+            {
+                if (hit.transform.gameObject.tag =="LinkObject" )
+                {
+                    itemManager = GameObject.FindWithTag("ItemManager");
+                    int allItems = itemManager.transform.childCount;
+                    for (int i = 0; i < allItems; i++)
+                    {
+                        if (itemManager.transform.GetChild(i).gameObject.GetComponent<Item>().id == id && itemManager.transform.GetChild(i).gameObject.GetComponent<Item>().equipped)
+                        {
+                            obj = itemManager.transform.GetChild(i).gameObject;  
+                            SlotHolder = GameObject.Find("Slot Holder");
+                            
+
+                            for (int j = 0; j < SlotHolder.transform.childCount; j++)
+                            {
+                                Slot = SlotHolder.transform.GetChild(j).gameObject;
+                                panel = Slot.transform.GetChild(0).gameObject;
+                                if (obj.GetComponent<Item>().icon == panel.GetComponent<Image>().sprite)
+                                {
+                                    Destroy(Slot);
+                                }
+                            }
+                            activeAction();
+                            Destroy(obj);          
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void activeAction()
+    {
+        if (obj.name == "Item book player")
+        {
+            Debug.Log("YEEEEES ça marche !!!");
+        }
+    }
+}
