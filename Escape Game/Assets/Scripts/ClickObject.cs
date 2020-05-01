7e14d8d52f9ea2fa39254e871c3fa0a95c64965b
Rob1Wagner <wag.rob@hotmail.fr>
2020-05-01 16:54:49 +0200

    personnages content, faché, triste et réfléchi

---
+++ Escape Game/Assets/Scripts/ClickObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickObject : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            
+            Touch touch = Input.GetTouch(0) ;
+<<<<<<< Updated upstream
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+=======
+            Ray ray1 = Camera.main.ScreenPointToRay(touch.position);
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition );
+            
+>>>>>>> Stashed changes
+            RaycastHit hit;
+            
+            
+            if( (Physics.Raycast(ray, out hit)) || (Physics.Raycast(ray1, out hit)))
+            {
+                
+<<<<<<< Updated upstream
+                if (touch.tapCount == 2 /*&& hit.transform.name == "Pass MS"*/)
+                {
+                    /*Debug.Log(hit.transform.name);
+                    SceneManager.LoadScene("the last revelation");*/
+                    if (hit.transform.name == "Cube Test")
+                    {
+                        SceneManager.LoadScene("Terminal Hacker");
+=======
+                if (Input.GetMouseButtonDown(0) || touch.tapCount == 2 )
+                {
+                    
+                    if (hit.transform.name == "Cube Test")
+                    {
+                        SceneManager.LoadScene("Terminal Hacker 1");
+                    }
+                    else if(hit.transform.name == "Pass MS")
+                    {
+                        SceneManager.LoadScene("the last revelation 1");
+>>>>>>> Stashed changes
+                    }
+                    else if(hit.transform.name == "Pass MS")
+                    {
+                        SceneManager.LoadScene("the last revelation");
+                    }
+                    else if (hit.transform.name == "Pass Initial")
+                    {
+                        SceneManager.LoadScene("Start scene");
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ bouche/Assets/triste.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triste : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Animator animateur = this.GetComponent<Animator>();
+            animateur.SetTrigger("anim_triste");
+        }
+    }
+}
