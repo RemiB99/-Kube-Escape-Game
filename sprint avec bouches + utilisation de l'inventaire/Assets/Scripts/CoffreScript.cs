6e1fbc50f067b12ef859a1b8d214300b8b286666
Hamze <hamzaalrashed1@hotmail.com>
2020-05-05 11:43:19 +0200

    ajout du champ (utilisation) dans l'inventaire

---
+++ sprint avec bouches + utilisation de l'inventaire/Assets/Scripts/CoffreScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffreScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(ray, out hit)){
+                
+                if (hit.transform.name == "Coffre") {
+                    
+                    this.GetComponent<Animator>().SetTrigger("Activate");
+                    this.GetComponent<BoxCollider>().enabled = false;
+                    Invoke("testDiams",2);
+            
+
+                    
+                }
+            } 
+        }
+    }
+
+    void testDiams(){
+        GameObject diams = GameObject.Find("Diamant");
+        if(diams != null){
+            diams.GetComponent<BoxCollider>().enabled = true;
+        }
+        
+    }
+}
